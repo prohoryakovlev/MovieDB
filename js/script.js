adb399e49e5937e6dfac07f9749b3ff0d9e6f11d
Prohor <prohor-yakovlev@mail.ru>
2022-03-26 19:48:11 +0300

    selector

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 /* Задания на урок:
 
 1) Удалить все рекламные блоки со страницы (правая часть сайта)
@@ -11,7 +12,6 @@
 Отсортировать их по алфавиту
 
 5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
@@ -23,4 +23,32 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    moviesList = document.querySelector('.promo__interactive-list');
+
+
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+moviesList.innerHTML = "";
+
+movieDB.movies.sort();
+
+
+movieDB.movies.forEach((film, i) => {
+    moviesList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
 
